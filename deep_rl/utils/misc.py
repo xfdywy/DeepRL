2c54bb86e5180665db7d3530e1496def7c7ebc7e
xfdywy <15118421@bjtu.edu.cn>
2019-05-28 00:18:52 -0700

    baseline bash 3

--- deep_rl/utils/misc.py
+++ deep_rl/utils/misc.py
@@ -19,12 +19,17 @@ def run_steps(agent):
     config = agent.config
     agent_name = agent.__class__.__name__
     t0 = time.time()
+    tmp = 0
     while True:
         if config.save_interval and not agent.total_steps % config.save_interval:
             agent.save('data/%s-%s-%d' % (agent_name, config.tag, agent.total_steps))
-        if config.log_interval and not agent.total_steps % config.log_interval:
-            agent.logger.info('steps %d, %.2f steps/s' % (agent.total_steps, config.log_interval / (time.time() - t0)))
-            t0 = time.time()
+        # if config.log_interval and not agent.total_steps % config.log_interval:
+        if config.log_interval:
+            new_tmp = int(agent.total_steps / config.log_interval)
+            if  new_tmp > tmp :
+                tmp  =  new_tmp
+                agent.logger.info('steps %d, %.2f steps/s' % (agent.total_steps, config.log_interval / (time.time() - t0)))
+                t0 = time.time()
         if config.eval_interval and not agent.total_steps % config.eval_interval:
             agent.eval_episodes()
         if config.max_steps and agent.total_steps >= config.max_steps:
